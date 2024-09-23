d6642615acb31578916973e838c8ae0ff4cd5a1a
DhrishP <parekhdhrish.pg@gmail.com>
2024-09-23 18:50:47 +0530

    add it to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,35 @@
-
-import Navbar from './(Header)/Navbar'
-import Herobody from './(Header)/Herobody'
-import About from './(About)/About'
-import Skill from './(Skills)/Skill'
-import Project from './(Projects)/Project'
-import Contact from './(Contact)/Contact'
-import AppNavbar from '@/components/app-navbar'
+import Navbar from "./(Header)/Navbar";
+import Herobody from "./(Header)/Herobody";
+import About from "./(About)/About";
+import Skill from "./(Skills)/Skill";
+import Project from "./(Projects)/Project";
+import Contact from "./(Contact)/Contact";
+import AppNavbar from "@/components/app-navbar";
+import Experience from "./(Experience)/Experience";
 
 export default function Home() {
   return (
     <>
-      <div className='h-screen w-screen bg-primary-foreground'>
-        <Navbar/>
-        <Herobody/>
-        <AppNavbar/>
+      <div className="h-screen w-screen bg-primary-foreground">
+        <Navbar />
+        <Herobody />
+        <AppNavbar />
       </div>
-      <div className='h-full md:h-[85vh] w-screen'>
-        <About/>
+      <div className="h-full md:h-[85vh] w-screen">
+        <About />
       </div>
-      <div className='h-full w-screen'>
-        <Skill/>
+      <div className="h-full w-screen">
+        <Skill />
       </div>
-      <div className='h-full w-screen'>
-        <Project/>
+      <div className="h-full w-screen">
+        <Experience />
       </div>
-      <div className='h-[80vh] md:h-full translate-y-24 w-screen'>
-        <Contact/>
+      <div className="h-full w-screen">
+        <Project />
+      </div>
+      <div className="h-[80vh] md:h-full translate-y-24 w-screen">
+        <Contact />
       </div>
     </>
-  )
+  );
 }
